a347dc890dfd7203458fb25d956f35d177ac8db1
Fernando <fernando@sanz-lazaro.com>
2021-10-23 10:58:37 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,23 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+requirements = []
+setup_requirements = []
+
+
 # This call to setup() does all the work
 setup(
     name="silabeador",
-    version="1.0.2-14",
-    description="Separa sílabas e identifica acentos",
+    version="1.0.2-15",
+    python_requires='>=3.5',
+    description="Syllabification and stress detection",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/fsanzl/silabeador",
+    project_urls={
+        'Source': 'https://github.com/fsanzl/silabeador/',
+        'Tracker': 'https://github.com/fsanzl/silabeador/issues',
+    },
     author="Fernando Sanz-Lázaro",
     author_email="fsanzl@gmail.com",
     license="LGPL",
@@ -22,7 +31,10 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.8", 
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
         "Natural Language :: Spanish",
     ],
     packages=["silabeador"],
